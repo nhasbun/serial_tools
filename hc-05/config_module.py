83a8e78f4288211414edcde826f9d08a8d324e11
Nicolas Hasbun <nhasbun@gmail.com>
2018-03-09 14:58:16 -0300

    Función para agregar ID a módulo de forma aleatoria.

--- hc-05/config_module.py
+++ hc-05/config_module.py
@@ -4,26 +4,34 @@
 from serial   import Serial
 from time     import sleep, time, strftime
 from datetime import datetime
+from os       import urandom # fn que genera un numero aleatorio de n bytes
+from binascii import b2a_hex # fn que convierte de bytes a hexadecimal
 
-
-t = datetime.fromtimestamp(time())
-DEVICE_NAME  = 'TAOTE ' + t.strftime('%d-%m-%Y %H:%M')
+## PARAMETERS ##
+BAUDRATE     = 38400 # Factory baudrate for AT mode
+COM_PORT     = 'COM31'
+DEVICE_NAME  = 'TAOTE '
+DEVICE_ID    = b2a_hex(urandom(4)).decode('utf-8')
 SET_BAUDRATE = 115200
 
+
+# ****************************************************************************
+# *    Función general para escribir comandos AT y esperar una respuesta.    *
+# ****************************************************************************
 
 def command(uart, command):
   print(command)
   command = command + '\r\n'
   command = command.encode()
   uart.write(command)
-  print(uart.readline().decode("utf-8"))
+  print(uart.readline().decode('utf-8'))
 
+
+# ****************************************************************************
+# *                                   MAIN                                   *
+# ****************************************************************************
 
 def main():
-
-  ## PARAMETERS ##
-  BAUDRATE = 38400 # Factory baudrate for AT mode
-  COM_PORT = 'COM31'
 
   uart = Serial(COM_PORT, BAUDRATE, timeout = 1)
 
@@ -32,7 +40,7 @@ def main():
 
   command(uart, 'AT')
   command(uart, 'AT+VERSION?')
-  command(uart, 'AT+NAME=' + DEVICE_NAME)
+  command(uart, 'AT+NAME=' + DEVICE_NAME + DEVICE_ID)
   command(uart, 'AT+UART=' + str(SET_BAUDRATE) + ',0,0')
 
   print("Configuration routine complete")
